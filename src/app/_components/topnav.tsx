b07e94f27600c335ce334c6745f999f6504d79c2
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-11 08:19:48 -0400

    Basic TopNav component

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,8 @@
+export default function TopNav() {
+  return (
+    <nav className="flex justify-between text-xl font-semibold border-b p-4 items-center">
+      <div>Gallery</div>
+      <div>Sign in</div>
+    </nav>
+  );
+}

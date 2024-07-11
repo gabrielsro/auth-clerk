0d52d4ec4a54bedaf46a897d5201d4d8ef67a1a4
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-11 16:28:04 -0400

    Added basic uploadthing image upload capabilities

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,20 +1,19 @@
-import {
-  SignInButton,
-  SignOutButton,
-  SignedIn,
-  SignedOut,
-} from "@clerk/nextjs";
+"use client";
+
+import { UploadButton } from "../utils/uploadthing";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function TopNav() {
   return (
     <nav className="flex justify-between text-xl font-semibold border-b p-4 items-center">
       <div>Auth Clerk</div>
-      <div>
+      <div className="flex gap-8">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <SignOutButton />
+          <UploadButton endpoint="imageUploader" />
+          <UserButton />
         </SignedIn>
       </div>
     </nav>

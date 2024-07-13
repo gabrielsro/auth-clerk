7d0ccdbd537d59581ca17bc46addb9efe4dd3854
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-07-13 07:08:29 -0400

    Break queries into separate file

---
+++ src/server/queries.ts
@@ -0,0 +1,16 @@
+//make sure file never runs on client-side:
+import "server-only";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export async function getMyImages() {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return images;
+}
